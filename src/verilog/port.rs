26eb5d9c9cbc33e17ebc82e3bd9093c04b555083
Lenslan <lenslan3@gmail.com>
2025-08-27 21:47:53 +0800

    feat: process undefine wire

--- src/verilog/port.rs
+++ src/verilog/port.rs
@@ -1,4 +1,5 @@
 use std::{ops::RangeInclusive, sync::Arc, vec};
+use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::ops::Range;
 use crate::verilog::port::VerilogValue::{Number, Wire};
@@ -29,17 +30,21 @@ impl VerilogPort {
         }
     }
 
-    fn connect_full_signal(&mut self, sig: &str) {
+    fn connect_wire(&self, sig: &str, range: &Range<usize>) -> Arc<VerilogWire> {
+        match self.inout {
+            PortDir::InPort => WireBuilder::add_load_wire(sig, range),
+            PortDir::OutPort => WireBuilder::add_driver_wire(sig, range),
+            _ => WireBuilder::add_load_wire(sig, range)             //TODO how to process inout port
+        }
+    }
+
+    fn connect_undefined_signal(&mut self, sig: &str) {
         self.signals.push(VerilogValue::UndefinedWire(sig.into()));
         self.has_undefine += 1;
     }
 
     fn connect_partial_signal(&mut self, sig: &str, range: &Range<usize>){
-        let wire = match self.inout {
-            PortDir::InPort => WireBuilder::add_load_wire(sig, range),
-            PortDir::OutPort => WireBuilder::add_driver_wire(sig, range),
-            _ => WireBuilder::add_load_wire(sig, range)             //TODO how to process inout port
-        };
+        let wire = self.connect_wire(sig, range);
         self.signals.push(Wire(Arc::clone(&wire), range.clone()));
     }
 
@@ -50,18 +55,57 @@ impl VerilogPort {
         });
     }
 
-
-    fn check_health(&self) -> anyhow::Result<()> {
-        todo!()
-        // for (bit, flag) in self.connected.iter().enumerate() {
-        //     if flag.is_none() {
-        //         log::warn!("Port {}[{}] has not connected", self.name, bit)
-        //     }
-        // }
+    fn get_connected_width(&self) -> usize {
+        let mut width_sum = 0;
+        for sig in self.signals.iter() {
+            match sig {
+                Wire(_, range) => {width_sum += range.len()},
+                Number { width, value } => {width_sum += width_sum},
+                _ => {}
+            }
+        }
+        width_sum
     }
 
-    pub fn get_info(&self) -> &str {
-        &self.info
+    fn set_undefine_wire(&mut self) {
+        let width_sum = self.get_connected_width();
+        let wire_infer_width = self.width - width_sum;
+        if wire_infer_width <= 0 {
+            log::warn!("Port {} has been over connected", self.name);
+            return ;
+        }
+        let (idx, signal) = self.signals
+            .iter()
+            .enumerate()
+            .find(|&sig| sig.1.is_undefine()).unwrap();
+        let arc_wire = self.connect_wire(
+            signal.get_name(),
+            &(0..wire_infer_width)
+        );
+        self.signals[idx] = Wire(Arc::clone(&arc_wire), 0..wire_infer_width);
+        self.has_undefine -= 1;
+    }
+
+    fn check_connected(&self) {
+        let width_sum = self.get_connected_width();
+        match self.width.cmp(&width_sum) {
+            Ordering::Greater => log::warn!("Port {} has not been full connected", self.name),
+            Ordering::Less => log::warn!("Port {} has been over connected", self.name),
+            _ => {}
+        }
+    }
+
+    fn register_undefine_wire(&self) {
+        todo!()
+    }
+
+
+    fn check_health(&mut self) {
+        match self.has_undefine {
+            0 => self.check_connected(),
+            1 => self.set_undefine_wire(),
+            _ => self.register_undefine_wire()
+        };
     }
 
 
@@ -114,6 +158,21 @@ impl VerilogValue {
             _ => false
         }
     }
+
+    fn is_undefine(&self) -> bool {
+        match self {
+            Self::UndefinedWire(_) => true,
+            _ => false
+        }
+    }
+
+    fn get_name(&self) -> &str {
+        match self {
+            Self::UndefinedWire(s) => s,
+            Self::Wire(w, _) => &w.name,
+            _ => ""
+        }
+    }
 }
 
 #[cfg(test)]
@@ -124,7 +183,7 @@ mod test {
     fn test_port() {
         simple_logger::init_with_level(log::Level::Info).unwrap();
         let mut port1 = VerilogPort::new(PortDir::InPort, "port1", 6);
-        port1.connect_full_signal("wire1");
+        port1.connect_undefined_signal("wire1");
         let mut port2 = VerilogPort::new(PortDir::OutPort, "port2", 6);
         port2.connect_partial_signal("wire1", &(0..3));
 
