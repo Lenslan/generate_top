05954148832a4f2a6ce7f3a0ac28c0a7d0dc4c00
Lenslan <lenslan3@gmail.com>
2025-08-29 17:23:14 +0800

    nothing to do

--- src/verilog/parse.rs
+++ src/verilog/parse.rs
@@ -1,5 +1,24 @@
-// to use verible or pyverilog
-// verilog-parser
-// https://github.com/ben-marshall/verilog-parser?tab=readme-ov-file
-// Pyverilog
-// https://github.com/PyHDI/Pyverilog
+/// to use verible or pyverilog
+///
+/// verilog-parser
+/// https://github.com/ben-marshall/verilog-parser?tab=readme-ov-file
+///
+/// Pyverilog
+/// https://github.com/PyHDI/Pyverilog
+///      - need iverilog to preprocess
+///
+/// verible
+/// https://github.com/chipsalliance/verible?tab=readme-ov-file
+///
+
+struct VeribleVerilogSyntax {
+    executable: String,
+}
+
+impl VeribleVerilogSyntax {
+    pub fn new(executable: &str) -> Self {
+        Self {
+            executable: executable.to_string(),
+        }
+    }
+}
