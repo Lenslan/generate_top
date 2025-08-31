df22c7730751802e304abab8de81315b267aa320
Lenslan <lenslan3@gmail.com>
2025-08-31 22:54:42 +0800

    add verilog test file& parse bin file

--- src/verilog/parse.rs
+++ src/verilog/parse.rs
@@ -1,3 +1,7 @@
+use std::process::Command;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
 /// to use verible or pyverilog
 ///
 /// verilog-parser
@@ -20,5 +24,51 @@ impl VeribleVerilogSyntax {
         Self {
             executable: executable.to_string(),
         }
+    }
+
+    fn run(&self) {
+        let output = Command::new(&self.executable)
+            .args(["-export_json", "-printtree"])
+            .arg("test/npu_afifo_r.sv")
+            .output()
+            .expect("can not exec script");
+
+        let v:Value = serde_json::from_slice(&output.stdout).unwrap();
+        for (file_path, file_data) in v.as_object().unwrap() {
+            println!("{}", file_path);
+            let temp:VeribleErrors = serde_json::from_value(file_data.clone()).unwrap();
+            println!("{:?}", temp);
+
+        }
+
+        println!("status {}", output.status);
+        println!("status: {}", output.status.success());
+        // println!("output: {}", String::from_utf8_lossy(&output.stdout));
+        // println!("error: {}", String::from_utf8_lossy(&output.stderr));
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Errors {
+    column: u32,
+    line: u32,
+    phase: String,
+    text: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct VeribleErrors {
+    errors: Vec<Errors>
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_command() {
+        let v = VeribleVerilogSyntax::new("bin/verible-verilog-syntax.exe");
+        v.run()
+
     }
 }
